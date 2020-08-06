793a0ffe4de0e7ec8cec4bae95b0936a3e1882d1
Mig Tam Rod <migtam628@gmail.com>
2020-08-06 00:04:40 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var express = require('express')
 var fs = require('fs')
-var subtitles = express()
+var app = express()
 var yifysubtitles = require('yifysubtitles')
 var functions = require('firebase-functions')
 var port = 5000;
 
-subtitles.use((req, res, next) => {
+app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.set({
@@ -15,7 +15,7 @@ subtitles.use((req, res, next) => {
   next()
 })
 
-subtitles.get('/', (req, res) => {
+app.get('/', (req, res) => {
   let id = req.query.id
   let language = req.query.language
   console.log(language)
@@ -50,50 +50,6 @@ subtitles.get('/', (req, res) => {
   }
 })
 
-subtitles.listen(port)
+app.listen(port)
 
-// subtitles.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', 'YOUR-DOMAIN.TLD') // update to match the domain you will make the request from
-//   res.header(
-//     'Access-Control-Allow-Headers',
-//     'Origin, X-Requested-With, Content-Type, Accept',
-//   )
-//   next()
-// })
-
-// subtitles.get('/', (req, res) => {
-//   let id = req.query.id
-//   let language = req.query.language
-//   console.log(language)
-//   let results = search(id, language).then((res) => {
-//     return res
-//   })
-//   async function search(id, language) {
-//     const results = await yifysubtitles(id, {
-//       path: '/tmp/',
-//       // langs: ['ar', 'zh', 'en', 'fr', 'de', 'he', 'it', 'ja', 'pt', 'ru', 'es'],
-//       langs: [language],
-//     })
-//       .then((res) => {
-//         return res
-//       })
-//       .then((res) => {
-//         return res
-//       })
-//     let shortLang = results[0].langShort
-//     let lang = results[0].lang
-//     let path = results[0].path
-
-//     fs.readFile(path, { encoding: 'utf-8' }, (err, data) => {
-//       if (!err) {
-//         res.writeHead(200, { 'Content-Type': 'application/json' })
-//         res.write(data)
-//         res.end()
-//       } else {
-//         console.log(err)
-//       }
-//     })
-//     // res.send(file)
-//   }
-// })
 
