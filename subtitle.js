8c3b8f16b5ed7787d0bf8cc42f29e151c762610a
Mig Tam Rod <migtam628@gmail.com>
2020-08-31 23:38:45 -0400

    Update subtitle.js
    removed yifysubtitles;
    added opensubtitles;
    added Movies and TV episodes subtitle support;

--- subtitle.js
+++ subtitle.js
@@ -2,28 +2,49 @@ const fs = require("fs");
 const yifysubtitles = require("yifysubtitles");
 
 module.exports = async (req, res) => {
-  let id = req.query.id;
-  let language = req.query.lang;
-  console.log({ id, language });
-  const results = await yifysubtitles(id, {
-    path: "/tmp/",
-    langs: [language],
-  }).then((res) => {
-    console.log(res);
-    return res;
-  });
+  let query = req.query.query;
+	let tmdbid = req.query.tmdbid;
+	let imdbid = req.query.imdbid;
+	let s = req.query.s;
+	let e = req.query.e;
+	let language = req.query.lang;
+	const OpenSubtitles = new OS({
+		useragent: 'migtamrod',
+		username: 'migtamrod',
+		password: '99009900Mm.',
+		ssl: true
+	});
 
-  let shortLang = results[0].langShort;
-  let lang = results[0].lang;
-  let path = results[0].path;
-  console.log(path);
-  fs.readFile(path, { encoding: "utf-8" }, (err, data) => {
-    if (!err) {
-      res.writeHead(200, { "Content-Type": "text/vtt" });
-      res.write(data);
-      res.end();
-    } else {
-      console.log(err);
-    }
-  });
+	let file_name = 'temp_sub.vtt';
+  var subtitles
+	await OpenSubtitles.search({
+		imdbid: imdbid,
+		sublanguageid: language,
+		season: s,
+		episode: e,
+		extensions: ['srt', 'vtt'],
+		limit: '1',
+		tmdbid: tmdbid,
+		query: query
+	})
+		.then((sub) => {
+			const file = fs.createWriteStream(file_name);
+			let sub_url = sub[Object.keys(sub)[0]][0].vtt;
+			const request = https.get(sub_url, (response) => {
+				response.pipe(file);
+			});
+		})
+		.catch((e) => res.send(e));
+  
+  setTimeout(() => {
+				fs.readFile('./' + file_name, (err, data) => {
+					if (err) res.send(err);
+					else {
+						res.writeHead(200, { 'Content-Type': 'text/vtt' });
+						res.write(data);
+						res.end();
+					}
+				});
+			}, 700);
+  
 };
